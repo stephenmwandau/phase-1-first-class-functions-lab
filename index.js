89333590b5bb1ab446e53fada1b2280ee6f91ee4
stephenmwandau <stephen.mwandau@student.moringaschool.com>
2024-06-27 15:18:50 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  // Function to return the last two drivers from an array
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // Array containing the above two functions
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Function to create a fare multiplier
+  const createFareMultiplier = function(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  };
+  
+  // Functions to double and triple fares
+  const fareDoubler = createFareMultiplier(2);
+  const fareTripler = createFareMultiplier(3);
+  
+  // Function to select different drivers based on the function passed
+  const selectDifferentDrivers = function(drivers, fn) {
+    return fn(drivers);
+  };
+  
